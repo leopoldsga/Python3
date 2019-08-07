8b7af4c54f89461cee3d4e9a4761b0d651ec275c
Guoao Sun <51565532+Guoao-Sun@users.noreply.github.com>
2019-08-07 10:49:33 +0800

    Create Dynamically_change_label_text_&_count.py
    This python code does the following things:
    1. The text of the label changes, which is triggered by the registered radio-button function.
    2. Another label reflects the times of the 'hello' label being changed.

---
+++ GUI-tkinter/Dynamically_change_label_text_&_count.py
@@ -0,0 +1,39 @@
+#Dynamically change the text of one label and
+#count the changed times.
+#Author email: guoaosun@outlook.com
+from tkinter import Tk, Checkbutton, Label,Radiobutton
+from tkinter import StringVar, IntVar
+
+root = Tk()
+
+text = StringVar()
+text.set('Hello, world')
+status = IntVar()
+counter  = StringVar()
+cnt = 0
+counter.set(str(cnt)+ " times")
+
+def count():
+    global counter, cnt
+    cnt+=1
+    counter.set(str(cnt) + " times")
+
+
+def change():
+    if status.get() == 1:   # if clicked
+        text.set('Hello, Mack')
+        count()
+    else:
+        text.set('Hello, Maria')
+        count()
+
+cb = Radiobutton(root, text = 'Mack',variable=status,value = 1, command=change)
+cb2 = Radiobutton(root, text = 'Maria' ,variable=status, value =2, command=change)
+lb = Label(root, textvariable=text, fg = 'green')
+label = Label(root, textvariable = counter)
+cb.pack()
+cb2.pack()
+lb.pack()
+label.pack()
+
+root.mainloop()
