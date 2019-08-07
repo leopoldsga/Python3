729a89f5839ee437999e8f9815e887bd5fa1d855
Guoao Sun <51565532+Guoao-Sun@users.noreply.github.com>
2019-08-07 10:55:08 +0800

    Update Dynamically_change_label_text_&_count.py
    Use label config to change its text.

--- GUI-tkinter/Dynamically_change_label_text_&_count.py
+++ GUI-tkinter/Dynamically_change_label_text_&_count.py
@@ -1,6 +1,8 @@
 #Dynamically change the text of one label and
 #count the changed times.
 #Author email: guoaosun@outlook.com
+
+##Version 1-------------------------------------------
 from tkinter import Tk, Checkbutton, Label,Radiobutton
 from tkinter import StringVar, IntVar
 
@@ -37,3 +39,41 @@ lb.pack()
 label.pack()
 
 root.mainloop()
+##--------------------------------------------------------------------
+
+##Version 2-----------------------------------------------------------
+from tkinter import Tk, Checkbutton, Label,Radiobutton
+from tkinter import StringVar, IntVar
+
+root = Tk()
+
+text = StringVar()
+text.set('Hello, world')
+status = IntVar()
+cnt = 0
+
+def count():
+    global label, cnt
+    cnt+=1
+    label.config(text = str(cnt) + " times")
+
+
+def change():
+    if status.get() == 1:   # if clicked
+        text.set('Hello, Mack')
+        count()
+    else:
+        text.set('Hello, Maria')
+        count()
+
+cb = Radiobutton(root, text = 'Mack',variable=status,value = 1, command=change)
+cb2 = Radiobutton(root, text = 'Maria' ,variable=status, value =2, command=change)
+lb = Label(root, textvariable=text, fg = 'green')
+label = Label(root)
+cb.pack()
+cb2.pack()
+lb.pack()
+label.pack()
+
+root.mainloop()
+##--------------------------------------------------------------
